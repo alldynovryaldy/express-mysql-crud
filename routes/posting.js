2cafe4a8d06245cf17c65cc0add4457f5d4dbc18
alldynovryaldy <alldy1400018051@webmail.uad.ac.id>
2022-04-26 09:22:58 +0700

    INSERT

--- routes/posting.js
+++ routes/posting.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const posting = express.Router();
-
 const connection = require('../config/database');
+const { body, validationResult } = require('express-validator');
 
 // index
 posting.get('/posting', function (req, res) {
@@ -23,5 +23,43 @@ posting.get('/posting', function (req, res) {
     }
   );
 });
+
+// insert
+posting.post(
+  '/posting',
+  [body('title').notEmpty(), body('content').notEmpty()],
+  (req, res) => {
+    // Finds the validation errors in this request and wraps them in an object with handy functions
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+
+    // define formData
+    let formData = {
+      title: req.body.title,
+      content: req.body.content,
+    };
+
+    connection.query(
+      'INSERT INTO posting SET ?',
+      formData,
+      function (err, rows) {
+        if (err) {
+          return res.status(500).json({
+            status: false,
+            message: 'Internal Server Error',
+          });
+        } else {
+          return res.status(200).json({
+            status: true,
+            message: 'Tambah data berhasil.',
+            data: rows,
+          });
+        }
+      }
+    );
+  }
+);
 
 module.exports = posting;
--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
-// import routes
 const routes = require('./routes');
+const bodyParser = require('body-parser');
 
-app.use(routes); // use routes
+app.use(
+  bodyParser.json(), // parse application/json
+  bodyParser.urlencoded({ extended: false }), // parse application/x-www-form-urlencoded
+  routes // use routes
+);
 
 app.listen(port, () => {
   console.log(`Server running in port ${port} ...`);
