e24fa2ee9f2fcb2b21225d0b8ac74271a07ae2e7
alldynovryaldy <alldy1400018051@webmail.uad.ac.id>
2022-04-26 10:59:58 +0700

    update cors

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const app = express();
 const port = 3000;
 const routes = require('./routes');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 app.use(
+  cors(), // cors
   bodyParser.json(), // parse application/json
   bodyParser.urlencoded({ extended: false }), // parse application/x-www-form-urlencoded
   routes // use routes
