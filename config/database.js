28c8dbd365283037ac528c2e34d8a57cf1a5b292
alldynovryaldy <alldy1400018051@webmail.uad.ac.id>
2022-04-27 13:19:09 +0700

    install & config dotENV

--- config/database.js
+++ config/database.js
@@ -2,10 +2,10 @@ const mysql = require('mysql');
 
 // config database
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'express-mysql-crud',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 connection.connect(function (err) {
